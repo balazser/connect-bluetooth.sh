007e262df2dfb3e62e91d64a59a50b9958eb4736
Balazs Erdos <erdos.balazs@live.com>
2023-02-17 13:26:08 +0100

    add --pair flag

--- README.md
+++ README.md
@@ -6,3 +6,11 @@ This script uses the `bluetoothctl` to connect to a Bluetooth device using its M
 
 To use the script, save the script to a file, make it executable using `chmod +x <filename>`, and then run the script with the MAC address of the device you want to connect to as an argument.
 
+`Usage: bluetooth-connect.sh [--pair] <MAC address>`
+
+- `--pair`: Automatically pair with the device if it's not already paired.
+- `<MAC address>`: The MAC address of the Bluetooth device to connect to.
+
+## License
+
+This project is licensed under the MIT License. See the [LICENSE](LICENSE) file for details.
--- connect-bluetooth.sh
+++ connect-bluetooth.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 
 if [ $# -eq 0 ]; then
-    echo "Usage: $0 <MAC address>"
+    echo "Usage: $0 [--pair] <MAC address>"
     exit 1
 fi
 
-# Get the Bluetooth device MAC address from the first argument
-DEVICE_MAC_ADDRESS="$1"
+# Get the Bluetooth device MAC address from the last argument
+DEVICE_MAC_ADDRESS="${@: -1}"
 
 # Check if the device is already paired
 if ! bluetoothctl paired-devices | grep -q "$DEVICE_MAC_ADDRESS"; then
-    echo "The device with MAC address $DEVICE_MAC_ADDRESS is not paired yet. Please pair it first and try again."
-    exit 1
+    if [ "$1" = "--pair" ]; then
+        echo "Pairing with the device with MAC address $DEVICE_MAC_ADDRESS..."
+        echo -e "pair $DEVICE_MAC_ADDRESS\nquit" | bluetoothctl
+    else
+        echo "The device with MAC address $DEVICE_MAC_ADDRESS is not paired yet. Please pair it first and try again."
+        exit 1
+    fi
 fi
 
 # Connect to the device using bluetoothctl
+echo "Connecting to the device with MAC address $DEVICE_MAC_ADDRESS..."
 echo -e "connect $DEVICE_MAC_ADDRESS\nquit" | bluetoothctl
